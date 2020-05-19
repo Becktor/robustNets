3d002a389d3d11916b712e865f67f3e54a9cc1c8
Jonathan Becktor <JonathanBecktor@gmail.com>
2020-05-19 12:30:26 +0200

    updates

--- retinanet/anchors.py
+++ retinanet/anchors.py
@@ -16,7 +16,7 @@ class Anchors(nn.Module):
         if ratios is None:
             self.ratios = np.array([0.76, 1.13, 1.58, 2.18, 3.2],)
         if scales is None:
-            self.scales = np.array([0.5, 1, 2])
+            self.scales = np.array([0.25, 0.5, 1])
 
     def forward(self, image):
 
