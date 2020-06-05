7b312c64066e127007f69268abd9ddbc5255a926
Jonathan Becktor <JonathanBecktor@gmail.com>
2020-06-05 10:09:57 +0200

    rm redundant import

--- train.py
+++ train.py
@@ -70,7 +70,7 @@ def main(args=None):
         model = retinanet.resnet34(num_classes=dataset_train.num_classes(), pretrained=pre_trained)
     elif parser.depth == 50:
         model = retinanet.resnet50(num_classes=dataset_train.num_classes(), pretrained=pre_trained,
-                                   act=GroupSort(8, axis=1), spectral_norm=True)
+                                   act=GroupSort(4, axis=1), spectral_norm=True)
     elif parser.depth == 101:
         model = retinanet.resnet101(num_classes=dataset_train.num_classes(), pretrained=pre_trained)
     elif parser.depth == 152:
