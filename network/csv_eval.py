fixed bugs in recall/precision prints

network/csv_eval.py
@@ -235,10 +235,13 @@ def evaluate(
         average_precision = _compute_ap(recall, precision)
         average_precisions[label] = average_precision, num_annotations
 
-    print('\nmAP:')
+    print('\nAP:')
+    map_list = []
     for label in range(generator.num_classes()):
         label_name = generator.label_to_name(label)
+        map_list.append(average_precisions[label][0])
         print('{}: {}'.format(label_name, average_precisions[label][0]))
         return_list.append((label_name, average_precisions[label][0]))
+    print('\nmAP: {}'.format(np.mean(map_list)))
     print('')
     return average_precisions, return_list