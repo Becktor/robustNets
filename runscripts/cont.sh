a37ef2c730cb5020504d0eecdb87f022f557f5a2
Jonathan Becktor <JonathanBecktor@gmail.com>
2020-06-25 10:16:08 +0200

    mend

--- runscripts/cont.sh
+++ runscripts/cont.sh
@@ -22,11 +22,11 @@
 #BSUB -N
 ### -- Specify the output and error file. %J is the job-id --
 ### -- -o and -e mean append, -oo and -eo mean overwrite --
-#BSUB -o spectral_gs2.out
-#BSUB -e spectral_gs2.err
+#BSUB -o gs2.out
+#BSUB -e gs2.err
 # -- end of LSF options --
 # Load the cuda module
 module load python3/3.6.7
 module load cudnn/v7.6.5.32-prod-cuda-10.0
 source /work1/jbibe/venv/bin/activate
-python train.py --csv_train /work1/jbibe/mmdet/fix_annotations_rgb_train.csv --csv_classes classes.csv --csv_val /work1/jbibe/mmdet/fix_annotations_rgb_val.csv --batch_size 2 --continue_training /work1/jbibe/git/robustNets/trained_models/model167_2057
+python train.py --csv_train /work1/jbibe/mmdet/fix_annotations_rgb_train.csv --csv_classes classes.csv --csv_val /work1/jbibe/mmdet/fix_annotations_rgb_val.csv --batch_size 2 --continue_training /work1/jbibe/git/robustNets/trained_models/model176_0858
