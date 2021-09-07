9830603c92aa6431db74a2a7afa993408820284b
Jonathan Becktor <jbibe@elektro.dtu.dk>
2021-09-07 22:11:09 +0200

    runscript for big

--- runscripts/start.og.sh
+++ runscripts/start.og.sh
@@ -1,33 +1,3 @@
-#!/bin/sh
-### General options
-### -- specify queue --
-#BSUB -q gpuv100
-### -- set the job Name --
-#BSUB -J reweight
-### -- ask for number of cores (default: 1) --
-#BSUB -n 4
-### -- Select the resources: 1 gpu in exclusive process mode --
-#BSUB -gpu "num=1:mode=exclusive_process"
-### -- set walltime limit: hh:mm -- maximum 24 hours for GPU-queues right now
-#BSUB -W 24:00
-# request 5GB of system-memory
-#BSUB -R "rusage[mem=32GB]"
-#BSUB -R "select[gpu32gb]"
-### -- set the email address --
-# please uncomment the following line and put in your e-mail address,
-# if you want to receive e-mail notifications on a non-default address
-#BSUB -u jbibe@elektro.dtu.dk
-### -- send notification at start --
-#BSUB -B
-### -- send notification at completion--
-#BSUB -N
-### -- Specify the output and error file. %J is the job-id --
-### -- -o and -e mean append, -oo and -eo mean overwrite --
-#BSUB -o reweight_%J.out
-#BSUB -e reweight_%J.err
-# -- end of LSF options --
-# Load the cuda module
-module load python3/3.6.7
-module load cudnn/v7.6.5.32-prod-cuda-10.0
-source /work1/jbibe/venv/bin/activate
-python train.py --csv_train /work1/jbibe/orig.csv --csv_classes classes.csv --csv_val /work1/jbibe/mmdet/fix_annotations_rgb_val.csv --csv_weight /work1/jbibe/weightset_hpc.csv --batch_size=32
+#!/bin/bash
+source /home/jobe/venv/bin/activate
+python train.py --csv_train /home/jobe/sftp/transfer/mmdet/annotations_rgb_train_big.csv --csv_classes /home/jobe/sftp/transfer/mmdet/classes2.csv --csv_val /home/jobe/sftp/transfer/mmdet/annotations_rgb_val_big.csv --csv_weight /home/jobe/sftp/transfer/weightset_big.csv --batch_size=32
