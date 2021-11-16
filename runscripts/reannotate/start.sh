d3c6c44d66a6b23c09494b0f4c5b0d099090d0bb
Jonathan <JonathanBecktor@gmail.com>
2021-11-16 18:27:21 +0100

    runscripts

--- runscripts/reannotate/start.sh
+++ runscripts/reannotate/start.sh
@@ -30,4 +30,4 @@
 module load python3/3.6.7
 module load cudnn/v7.6.5.32-prod-cuda-10.0
 source /work1/jbibe/venv/bin/activate
-python train.py --csv_train /work1/jbibe/datasets/dataset_csvs/reannotation_set_hpc.csv --csv_classes classes.csv --csv_val /work1/jbibe/datasets/dataset_csvs/reannotation_valset_hpc.csv --csv_weight /work1/jbibe/datasets/dataset_csvs/weightset_85.csv --batch_size=16 --depth=50 --flip_mod 0 --rew_start 200
+python train.py --csv_train /work1/jbibe/datasets/dataset_csvs/reannotation_set_hpc.csv --csv_classes classes.csv --csv_val /work1/jbibe/datasets/dataset_csvs/reannotation_valset_hpc.csv --csv_weight /work1/jbibe/datasets/dataset_csvs/weightset_85.csv --batch_size=16 --depth=50 --flip_mod 0 --rew_start 200 --continue_training trained_models/auspicious-feast-167_1ysx8m27
