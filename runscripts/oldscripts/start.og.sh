5ce62472ba08975931bf17a64008a8a624ea1e42
Jonathan <JonathanBecktor@gmail.com>
2021-12-20 13:10:22 +0100

    removed runscripts from git

--- runscripts/oldscripts/start.og.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source /home/jobe/venv/bin/activate
-python train.py --csv_train /home/jobe/sftp/transfer/mmdet/annotations_rgb_train_big.csv --csv_classes /home/jobe/sftp/transfer/mmdet/classes2.csv --csv_val /home/jobe/sftp/transfer/mmdet/annotations_rgb_val_big.csv --csv_weight /home/jobe/sftp/transfer/weightset_big.csv --batch_size=32
